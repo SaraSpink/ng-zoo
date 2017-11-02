ea61663662e383258c9c41adb7287c28b43889bd
Sara Spink <spinkbot@gmail.com>
2017-11-01 18:54:19 -0700

    fix layout of new animal form and make sure add animal is working

--- app/new-animal.component.ts
+++ app/new-animal.component.ts
@@ -11,27 +11,38 @@ import { Animal } from './animal.model';
   <div class="well">
     <table class="table">
       <thead>
-        <th>Enter Species</th>
         <th>Enter Animal's Name</th>
+        <th>Enter Species</th>
         <th>Enter Animal's Age</th>
-        <th>Carnivore or Herbivore?</th>
-        <th>Location</th>
+      </thead>
+      <tbody>
+        <td><input #newAnimalName></td>
+        <td><input #newAnimalSpecies></td>
+        <td><input #newAnimalAge></td>
+      </tbody>
+
+      <thead>
         <th>Number of caretakers</th>
+        <th>Location</th>
         <th>Sex</th>
+      </thead>
+      <tbody>
+        <td><input #newAnimalCaretakers></td>
+        <td><input #newAnimalLocation></td>
+        <td><input #newAnimalSex></td>
+      </tbody>
+
+      <thead>
+        <th placeholder="Carnivore or Herbivore">Diet</th>
         <th>Likes</th>
         <th>Dislikes</th>
       </thead>
       <tbody>
-        <td><input #newAnimalSpecies></td>
-        <td><input #newAnimalName></td>
-        <td><input #newAnimalAge></td>
         <td><input #newAnimalDiet></td>
-        <td><input #newAnimalLocation></td>
-        <td><input #newAnimalCaretakers></td>
-        <td><input #newAnimalSex></td>
         <td><input #newAnimalLikes></td>
         <td><input #newAnimalDislikes></td>
       </tbody>
+
       <button (click)="submitForm(
         newAnimalSpecies.value,
         newAnimalName.value,
@@ -42,7 +53,6 @@ import { Animal } from './animal.model';
         newAnimalSex.value,
         newAnimalLikes.value,
         newAnimalDislikes.value);
-
         "
         class="btn btn-info">Add Animal</button>
     </table>
